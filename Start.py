ed23fadf4623fa74baea35bf4d838c78250d616b
simona-cyber <simona.flachsmann@gmail.com>
2025-02-26 08:58:15 +0100

    name nochmals geändert

--- Start.py
+++ Start.py
@@ -8,10 +8,8 @@ st.title("Meine erste Streamlit App")
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Albulena Ibishi (ibishalb@students.zhaw.ch)
+- Simona Flachsmann (flachsim@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
-"""
