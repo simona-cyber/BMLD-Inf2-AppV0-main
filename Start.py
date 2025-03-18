c7ef4cffb858a3713404d14f4714f147b1e2e63c
Albulena Ibishi <ibishalb@students.zhaw.ch>
2025-03-18 23:09:30 +0100

    LoginManager added

--- Start.py
+++ Start.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
 st.title("Meine erste Streamlit App")
 
 # Initialisierung des Data Managers (hier mit Verbindung zu SwitchDrive)
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Albumona")  
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Albumona") 
+
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
 
 # Laden der Daten
 data_manager.load_user_data(
