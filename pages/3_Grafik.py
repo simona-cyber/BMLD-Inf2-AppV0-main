2eb0b4eda67a13c21e2cfda119d6c80d1becd99e
simona-cyber <simona.flachsmann@gmail.com>
2025-03-20 18:28:50 +0100

    reihenfolge

--- pages/3_Grafik.py
+++ pages/3_Grafik.py
@@ -16,5 +16,5 @@ if data_df.empty:
 
 
 # Notendurchschnitte
-st.line_chart(data=data_df.set_index('beschreibungen')['weighted_average'], use_container_width=True)
+st.line_chart(data=data_df.set_index['weighted_average']('beschreibungen'), use_container_width=True)
 st.caption('Noten')
