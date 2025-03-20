0a9bc48518b82edeb2b4cb1ace75fe7fca8d726f
simona-cyber <simona.flachsmann@gmail.com>
2025-03-20 18:04:12 +0100

    grafik

---
+++ pages/3_Grafik.py
@@ -0,0 +1,21 @@
+# ====== Start Login Block ======
+from utils.login_manager import LoginManager
+LoginManager().go_to_login('Start.py')  
+# ====== End Login Block ======
+
+# ------------------------------------------------------------
+# === BMI Grafik ===
+import streamlit as st
+
+st.title('Notendurchschnitte')
+
+data_df = st.session_state['data_df']
+if data_df.empty:
+    st.info('Keine Notendurchschnitte vorhanden. Berechnen Sie Ihre Durchschnitte auf der Startseite.')
+    st.stop()
+
+# Notendurchschnitte
+st.line_chart(data=data_df.set_index('timestamp')['Note'], 
+                use_container_width=True)
+st.caption('Noten')
+
